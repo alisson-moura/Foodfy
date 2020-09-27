eb94a65c5b1b3b0d508305ce73ceff2703205ee4
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-09-27 14:01:07 -0300

    Adicionado dependencias e scripts

---
+++ routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const data = require('./data');
+
+const routes = express.Router();
+
+routes.get('/', function(req, res) {
+  return res.render('index.njk', {recipes: data});
+});
+
+routes.get('/recipes', function(req, res) {
+  return res.render('recipes.njk', {recipes: data});
+});
+
+routes.get('/recipe/:id', function(req, res) {
+  const id = req.params.id;
+  const recipe = data[id];
+
+  if(!recipe){
+    return res.send('Error 404');
+  }
+  return res.render('detailRecipe', {recipe});
+})
+
+routes.get('/about', function(req, res) {
+  return res.render('about.njk');
+});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,42 +1,26 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const methodOverride = require('method-override');
-const data = require('./data');
+const routes = require('./routes');
+
 
 const port = 5000;
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set("view engine", "njk");
+
 nunjucks.configure('views', {
   express: app,
   autoescape: false,
   noCache: true
 });
 
-app.use();
+app.use(methodOverride('_method'));
+app.use(routes)
 
-app.get('/', function(req, res) {
-  return res.render('index.njk', {recipes: data});
-});
-
-app.get('/recipes', function(req, res) {
-  return res.render('recipes.njk', {recipes: data});
-});
-
-app.get('/recipe/:id', function(req, res) {
-  const id = req.params.id;
-  const recipe = data[id];
-
-  if(!recipe){
-    return res.send('Error 404');
-  }
-  return res.render('detailRecipe', {recipe});
-})
-
-app.get('/about', function(req, res) {
-  return res.render('about.njk');
-});
 
 app.listen(port, function () {
-  console.log(`app running on port: ${port}`);
+  console.log(`app is running on port: ${port}`);
 });
