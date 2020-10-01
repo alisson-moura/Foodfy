36b7b6a8d8b2f484b38b97e007b8f37bfdfe38c4
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-09-30 22:28:17 -0300

    Alterado site principal para buscar dados do data.json

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,28 @@ const express = require('express');
 const adminRoutes = require('./admin');
 
 
-const data = require('../data');
+const data = require('../data.json');
 
 const routes = express.Router();
 
 routes.get('/', function(req, res) {
-  return res.render('index.njk', {recipes: data});
+  return res.render('index.njk', {recipes: data.recipes});
 });
 
 routes.use('/admin', adminRoutes);
 
 routes.get('/recipes', function(req, res) {
-  return res.render('recipes.njk', {recipes: data});
+  return res.render('recipes.njk', {recipes: data.recipes});
 });
 
 routes.get('/recipe/:id', function(req, res) {
-  const id = req.params.id;
-  const recipe = data[id];
+  const { id } = req.params;
+  const findRecipe = data.recipes.find(recipe => recipe.id === Number(id));
 
-  if(!recipe){
+  if(!findRecipe){
     return res.send('Error 404');
   }
-  return res.render('detailRecipe', {recipe});
+  return res.render('detailRecipe', {recipe: findRecipe});
 })
 
 routes.get('/about', function(req, res) {
