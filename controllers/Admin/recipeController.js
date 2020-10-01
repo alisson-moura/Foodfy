a433421f1cd2eb7eb02372c447fc1fb45e6c8e6f
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-09-30 22:21:43 -0300

    finalizado crud de receitas no arquivo .json

--- controllers/Admin/recipeController.js
+++ controllers/Admin/recipeController.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const data = require('../../data.json');
 
-exports.index = (req,res) => {
-  return res.render('Admin/index', {recipes: data.recipes});
+exports.index = (req, res) => {
+  return res.render('Admin/index', { recipes: data.recipes });
 }
 
 exports.create = (req, res) => {
   return res.render('Admin/create');
 }
 exports.post = (req, res) => {
-  const {image, title, ingredients, preparation, information} = req.body;
+  const { image, title, ingredients, preparation, information } = req.body;
   const keys = Object.keys(req.body);
 
   for (key of keys) {
@@ -19,24 +19,69 @@ exports.post = (req, res) => {
 
   let id = 1;
   const lastId = data.recipes[data.recipes.length - 1];
-  if(lastId){
+  if (lastId) {
     id = lastId.id + 1;
   }
 
-  data.recipes.push({id, image, title, ingredients, preparation, information, author: 'Alisson Moura' });
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-    if(err) return res.send('Erro ao escrever no arquivo');
+  data.recipes.push({ id, image, title, ingredients, preparation, information, author: 'Alisson Moura' });
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send('Erro ao escrever no arquivo');
 
     return res.send(data.recipes);
   });
 }
 
 exports.show = (req, res) => {
-  const {id} = req.params;
-
+  const { id } = req.params;
   const findRecipe = data.recipes.find(recipe => recipe.id === Number(id));
+  if (!findRecipe) return res.send('Receita não encontrada');
+  return res.render('Admin/show', { recipe: findRecipe });
+}
 
+exports.edit = (req, res) => {
+  const { id } = req.params;
+  const findRecipe = data.recipes.find(recipe => recipe.id === Number(id));
+  if (!findRecipe) return res.send('Receita não encontrada');
+  return res.render('Admin/edit', { recipe: findRecipe });
+}
 
+exports.put = (req, res) => {
+  const { id, title, image, ingredients, preparation, information } = req.body;
+  let index = 0;
 
-  return res.render('Admin/show', {recipe: findRecipe});
+  const findRecipe = data.recipes.find((recipe, RecipeIndex) => {
+    if (recipe.id === Number(id)) {
+      index = RecipeIndex;
+      return true;
+    }
+  });
+
+  if (!findRecipe) return res.send('Receita não encontrada');
+
+  data.recipes[index] = {
+    ...findRecipe,
+    id: Number(id), 
+    title, 
+    image, 
+    ingredients, 
+    preparation, 
+    information
+  }
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send('Erro ao escrever no arquivo');
+    return res.redirect(`/admin/recipes/${ data.recipes[index].id}`);
+  });
+}
+
+exports.delete = (req, res) => {
+  const {id} = req.body;
+  const filteredRecipes = data.recipes.filter(recipe => recipe.id != id);
+
+  data.recipes = filteredRecipes;
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+    if(err) return res.send('Erro ao gravar arquivo.')
+
+    return res.redirect('/admin');
+  });
 }
--- routes/admin.js
+++ routes/admin.js
@@ -11,4 +11,9 @@ adminRoutes.post('/recipes', recipeController.post);
 
 adminRoutes.get('/recipes/:id', recipeController.show);
 
+adminRoutes.get("/recipes/:id/edit", recipeController.edit);
+adminRoutes.put("/recipes", recipeController.put);
+
+adminRoutes.delete("/recipes", recipeController.delete);
+
 module.exports = adminRoutes;
