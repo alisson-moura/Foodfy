be02d833cae8bb330748ca82bb8f77dbd5e634eb
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-10-31 19:11:45 -0300

    Fix: Corrigido avatar preview

--- public/scripts/admin/scripts.js
+++ public/scripts/admin/scripts.js
@@ -4,3 +4,15 @@ const menuItems = document.querySelectorAll(".header a");
 for (const item of menuItems) {
   currentPage.includes(item.getAttribute('href')) ? item.classList.add("active") : '';
 }
+
+function readImage() {
+  if (this.files && this.files[0]) {
+      var file = new FileReader();
+      file.onload = function(e) {
+          document.getElementById("img-preview").src = e.target.result;
+      };       
+      file.readAsDataURL(this.files[0]);
+  }
+}
+
+document.getElementById("avatar").addEventListener("change", readImage, false);
