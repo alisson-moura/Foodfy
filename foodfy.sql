9065e602f879ebade22062e9d9e6b70dcc686651
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-12-09 08:31:44 -0300

    alterado estrutura do banco de dados

--- foodfy.sql
+++ foodfy.sql
@@ -1,10 +1,25 @@
 CREATE DATABASE foodfy;
 
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "email" text unique not null,
+  "password" text not null,
+  "reset_token" text,
+  "reset_token_expires" text,
+  "is_admin" boolean default false,
+  "file_id" int NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
 CREATE TABLE "chefs" (
   "id" SERIAL PRIMARY KEY,
   "name" varchar NOT NULL,
   "description" text,
   "file_id" int NOT NULL,
+  "email" text unique not null,
+  "password" text not null,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
@@ -32,6 +47,16 @@ CREATE TABLE "recipes_files" (
   "file_id" int NOT NULL
 );
 
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" 
+ADD CONSTRAINT "session_pkey" 
+PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
 ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id");
 
 ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DELETE CASCADE;
@@ -39,3 +64,4 @@ ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DEL
 ALTER TABLE "recipes_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "recipes_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DELETE CASCADE;
+
