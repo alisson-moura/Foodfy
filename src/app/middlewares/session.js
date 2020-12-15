fdd29d26bf539db2ddafff531f97df8b578ea7c6
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-12-15 08:02:34 -0300

    identado codigo

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,17 +1,17 @@
 function onlyAdmins(req, res, next) {
-    if(!req.session.userId) return res.redirect('/accounts');
+    if (!req.session.userId) return res.redirect('/accounts');
 
     next();
 }
 
-function onlyChefs(req,res, next) {
-    if(!req.session.chefId ||!req.session.userId ) return res.redirect('/accounts');
+function onlyChefs(req, res, next) {
+    if (!req.session.chefId || !req.session.userId) return res.redirect('/accounts');
 
     next();
 }
 
 function isLoggedRedirectToAdmin(req, res, next) {
-    if(req.session.userId || req.session.chefId) return res.redirect('/admin');
+    if (req.session.userId || req.session.chefId) return res.redirect('/admin');
 
     next();
 }
